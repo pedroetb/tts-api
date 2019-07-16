2d8631dc2e9cec940e529ddeb703b105f0753501
Pedro Trujillo <pedroetb@gmail.com>
2019-07-16 23:32:47 +0100

    Add robot voice effect

--- app/cmd.js
+++ app/cmd.js
@@ -154,7 +154,26 @@ function getSoxEffectsArgs(fields) {
 		}
 	}
 
+	if (fields.robot) {
+		args = args.concat(getSoxRobotVoiceArgs());
+	}
+
 	return args;
+}
+
+function getSoxRobotVoiceArgs() {
+
+	return [
+		'overdrive', '10',
+		'echo', '0.8', '0.8', '5', '0.7',
+		'echo', '0.8', '0.7', '6', '0.7',
+		'echo', '0.8', '0.7', '10', '0.7',
+		'echo', '0.8', '0.7', '12', '0.7',
+		'echo', '0.8', '0.88', '12', '0.7',
+		'echo', '0.8', '0.88', '30', '0.7',
+		'echo', '0.6', '0.6', '60', '0.7',
+		'gain', '8'
+	];
 }
 
 module.exports = {
